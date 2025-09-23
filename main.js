4f6de8bcfc7c0ea96ccd67d2deeaf55f2c7ac47d
VVVULTURE <148487197+VVVULTURE@users.noreply.github.com>
2025-09-22 20:30:48 -0400

    Update main.js

--- main.js
+++ main.js
@@ -15,6 +15,9 @@ Apps.texteditor = {
 
 // Import Games app logic from apps/games.js (do not redefine here!)
 
+// Fetcher (web browser, uses your fetch logic)
+Apps.fetcher = window.Apps.fetcher;
+
 // HTML Executor
 Apps.executor = {
   title: 'HTML Executor',
