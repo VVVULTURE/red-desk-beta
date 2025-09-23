96e8c3346d9004b35b2a4c567d304077af0519ba
VVVULTURE <148487197+VVVULTURE@users.noreply.github.com>
2025-09-22 20:24:49 -0400

    Update taskbar.js

--- taskbar.js
+++ taskbar.js
@@ -1,5 +1,5 @@
 window.Taskbar = {
-  pinned: ['texteditor', 'calculator', 'settings', 'terminal', 'fileexplorer', 'games', 'executor', 'websitefetcher'],
+  pinned: ['texteditor', 'calculator', 'settings', 'terminal', 'fileexplorer', 'games', 'executor'],
   running: [],
   init() {
     const bar = document.getElementById('taskbar');
